c391e21b84175b28a260c252cf425775676d689c
corylewis73 <corylewis73@gmail.com>
2022-12-29 19:48:24 -0500

    Best before date now defaults to current date.

--- ui/src/components/Inventory/InventoryFormModal.js
+++ ui/src/components/Inventory/InventoryFormModal.js
@@ -19,6 +19,14 @@ class InventoryFormModal extends React.Component {
         units
       } = this.props
 
+      const date = new Date();
+
+      let day = date.getDate();
+      let month = date.getMonth() + 1;
+      let year = date.getFullYear();
+      
+      let currentDate = `${year}-${month}-${day}`;
+
       return (
         <Dialog
         open={this.props.isDialogOpen}
@@ -93,6 +101,7 @@ class InventoryFormModal extends React.Component {
                         name='bestBeforeDate'
                         id='bestBeforeDate'
                         type='date'
+                        value={currentDate}
                         component='input'>
                       </Field>
                       <label for='neverExpires'>Never Expires: </label>
